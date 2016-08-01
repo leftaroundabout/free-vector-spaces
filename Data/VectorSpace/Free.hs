1dcd1ddfd3701dcb34774c2ff5440cf12ed6174f
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2016-08-01 18:20:57 +0200

    Initial commit

---
+++ Data/VectorSpace/Free.hs
@@ -0,0 +1,17 @@
+-- |
+-- Module      : Data.VectorSpace.Free
+-- Copyright   : (c) Justus Sagemüller 2016
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) sagemueller $ geo.uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+
+module Data.VectorSpace.Free (module Linear) where
+
+import Data.AffineSpace
+import Data.VectorSpace
+
+import Linear
+
